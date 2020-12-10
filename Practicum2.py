3b428cea35254a6e0a2a5a7d7d126d2b33f128e4
Dieterdemuynck <dieter.demuynck30@gmail.com>
2020-12-10 20:39:16 +0100

    Base classes initialised

--- Practicum2.py
+++ Practicum2.py
@@ -3,4 +3,17 @@ __author__ = "Dieter Demuynck" + " & " + "Rayan Verheecke"
 Practicum 2 voor Beginselen van Programmeren
 """
 
-# most likely gotta use classes for this one
+class Magazijn:
+    pass
+
+
+class ProductType:
+    pass
+
+
+class Klant:
+    pass
+
+
+class Bestelling:
+    pass
