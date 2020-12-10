72d9abbe5ae7f7a22d21bf78375842852fa3bbab
Dieterdemuynck <dieter.demuynck30@gmail.com>
2020-12-10 20:46:53 +0100

    __init__ voor Klant en Bestelling

--- Practicum2.py
+++ Practicum2.py
@@ -3,6 +3,7 @@ __author__ = "Dieter Demuynck" + " & " + "Rayan Verheecke"
 Practicum 2 voor Beginselen van Programmeren
 """
 
+
 class Magazijn:
     pass
 
@@ -12,8 +13,12 @@ class ProductType:
 
 
 class Klant:
-    pass
+    def __init__(self, naam, klant_id):
+        self._naam = naam
+        self._klant_id = klant_id
 
 
 class Bestelling:
-    pass
+    def __init__(self, aantal, producttype):
+        self._aantal = aantal
+        self._producttype = producttype
