65a6b4f0270abb80a1802eb9e5ed5df6690e38a5
rayanverheecke <rayan.verheecke@gmail.com>
2020-12-10 21:32:52 +0100

    voorlopige classes

--- Practicum2.py
+++ Practicum2.py
@@ -5,17 +5,29 @@ Practicum 2 voor Beginselen van Programmeren
 
 
 class Magazijn:
-    def __init__(self, ):
+    def __init__(self, producttypes, aantal = 0):
+        self._producttypes[producttype] = 0
+        self._aantal[] = aantal
 
+    def getAantal(self):
+        return self._aantal
+    def getProducttypes(self):
+        return self._producttypes
 
 
 class ProductType:
     def __init__(self, naam, aankoopprijs, verkoopprijs):
-        self._naam= naam
+        self._naam = naam
         self._aankoopprijs = aankoopprijs
         self._verkoopprijs = verkoopprijs
 
     def getNaam(self):
+        return self._naam
+    def getaankoopPrijs(self):
+        return self._aankoopprijs
+    def getverkoopPrijs(self):
+        return self._verkoopprijs
+
 
 
 
